ebfb2ceb76fa782831f18bfc2fcfd4494312d0b3
= <pierreambroise@hotmail.fr>
2021-09-15 16:48:50 +0900

    added some JS

--- source/assets/javascripts/index.js
+++ source/assets/javascripts/index.js
@@ -1,1 +1,28 @@
 import 'bootstrap';
+
+// This file is automatically compiled by Webpack, along with any other files
+// present in this directory. You're encouraged to place your actual application logic in
+// a relevant structure within app/javascript and only use these pack files to reference
+// that code so it'll be compiled.
+
+// Internal imports, e.g:
+// import { initSelect2 } from '../components/init_select2';
+
+// initSelect2();
+document.querySelectorAll('.card__inner').forEach((card) => card.addEventListener('click',
+  () => {
+    card.classList.toggle('is-flipped');
+  }));
+
+document.querySelectorAll('.description').forEach((card) => card.addEventListener('click',
+  () => {
+    this.classList.add('clear');
+  }));
+
+const descriptions = document.querySelectorAll('.description');
+
+// Loop through the elements.
+descriptions.forEach((card) => card.addEventListener('click',
+  () => {
+    card.classList.add('clear');
+  }));
