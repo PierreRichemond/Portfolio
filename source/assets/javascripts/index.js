38ae8f1804ab22c5b300cf2e2ee18a8e570b241f
= <pierreambroise@hotmail.fr>
2022-01-13 11:14:02 +0900

    added some css and JS

--- source/assets/javascripts/index.js
+++ source/assets/javascripts/index.js
@@ -1,14 +1,22 @@
 import 'bootstrap';
+import Typed from 'typed.js';
+import AOS from 'aos';
 
-// This file is automatically compiled by Webpack, along with any other files
-// present in this directory. You're encouraged to place your actual application logic in
-// a relevant structure within app/javascript and only use these pack files to reference
-// that code so it'll be compiled.
+if (document.getElementById('animate-fr')) {
+  new Typed ('#animate-fr', {
+    strings: ['La curiosité', 'Résoudre des problèmes', 'Le Travail d\'équipe', 'La Communication', 'La patience'],
+    typeSpeed: 70,
+    loop: true
+  });
+}
+if (document.getElementById('animate-en')) {
+  new Typed('#animate-en', {
+    strings: ['Cusiosity', 'Problem-solving', 'Teamwork', 'Communication', 'Patience'],
+    typeSpeed: 70,
+    loop: true
+  });
+}
 
-// Internal imports, e.g:
-// import { initSelect2 } from '../components/init_select2';
-
-// initSelect2();
 document.querySelectorAll('.card__inner').forEach((card) => card.addEventListener('click',
   () => {
     card.classList.toggle('is-flipped');
